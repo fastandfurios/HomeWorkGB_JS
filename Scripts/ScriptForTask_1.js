6f5d6bc0bebe4537993adebb25bf9b3083f00456
Дмитрий <semu-dima@yandex.ru>
2021-09-08 10:47:02 +0300

    :pencil2: added task1

---
+++ Scripts/ScriptForTask_1.js
@@ -0,0 +1,5 @@
+'use strict'
+
+const temperatureCels = prompt('Введите температуру в градусах Цельсия');
+const temperatureF = (9 / 5) * temperatureCels + 32;
+console.log(`Температура по Фаренгейту составит:  ${temperatureF} F`);
