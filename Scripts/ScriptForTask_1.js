f9a322e1fe3c9c3cffd278fab947001e401e7015
Дмитрий <semu-dima@yandex.ru>
2021-09-09 07:39:41 +0300

    :wrench: changed the script 1 and script 2

--- Scripts/ScriptForTask_1.js
+++ Scripts/ScriptForTask_1.js
@@ -1,5 +1,5 @@
 'use strict'
 
-const temperatureCels = prompt('Введите температуру в градусах Цельсия');
+const temperatureCels = +prompt('Введите температуру в градусах Цельсия');
 const temperatureF = (9 / 5) * temperatureCels + 32;
-console.log(`Температура по Фаренгейту составит:  ${temperatureF} F`);
+console.log(`Температура по Фаренгейту составит:  ${Math.round(temperatureF)} F`);
