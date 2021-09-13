f249f869b9ba8334d70ea293dfc83ab79480e30c
Дмитрий <semu-dima@yandex.ru>
2021-09-13 19:26:31 +0300

    :wrench: added jsdoc - task 5

--- Scripts/ScriptForTask_5.js
+++ Scripts/ScriptForTask_5.js
@@ -4,11 +4,17 @@ const a = +prompt('Введите число a:');
 const b = +prompt('Введите число b:');
 const operator = prompt('Введите одно из действий (+, -, /, *)');
 
-(isNaN(a) || isNaN(b)) 
+(Number.isNaN(a) || Number.isNaN(b)) 
     ? console.log('Было введено не число. Попробуйте снова.')
-    : selectActions(a, b, operator);
+    : outputResults(a, b, operator);
 
-function selectActions(a, b, operator) {
+/**
+ * В зависимости от введенного пользователем оператора, выводит результат
+ * @param {*} a - введенное пользователем число
+ * @param {*} b - введенное пользователем число
+ * @param {*} operator - введенный пользователем оператор
+ */
+function outputResults(a, b, operator) {
     switch (operator) {
         case '+':
             console.log(`Сумма ${a} и ${b} составит: ${getSum(a, b)}`)
@@ -22,21 +28,47 @@ function selectActions(a, b, operator) {
         case '*':
             console.log(`Произведение ${a} и ${b} составит: ${getInc(a, b)}`)
             break;
+        default:
+            console.log("Было введенно некорректное действие. Попробуйте снова.");
     }
 }
 
+/**
+ * складывает два числа
+ * @param {*} a - веденное пользоваетелем число
+ * @param {*} b - веденное пользоваетелем число
+ * @returns сумма двух чисел
+ */
 function getSum(a, b) {
     return a + b;
 }
 
+/**
+ * вычитает из числа a число b
+ * @param {*} a - веденное пользоваетелем число
+ * @param {*} b - веденное пользоваетелем число
+ * @returns разность двух чисел
+ */
 function getSub(a, b) {
     return a - b;
 }
 
+/**
+ * делит число a на число b
+ * @param {*} a - веденное пользоваетелем число
+ * @param {*} b - веденное пользоваетелем число
+ * @returns результат деления двух чисел
+ */
 function getDiv(a, b) {
     return (a / b).toFixed(2);
 }
 
+/**
+ * умножает два числа
+ * @param {*} a - веденное пользоваетелем число
+ * @param {*} b - веденное пользоваетелем число
+ * @returns произведение двух чисел
+ */
 function getInc(a, b) {
     return a * b;
 }
