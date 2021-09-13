96246e2b4edad1338fceb0c344f2d3acaeb09233
Дмитрий <semu-dima@yandex.ru>
2021-09-13 17:48:35 +0300

    :wrench: added ";", "{}" - task 3

--- Scripts/ScriptForTask_3.js
+++ Scripts/ScriptForTask_3.js
@@ -4,8 +4,14 @@ const a = +prompt('Введите число a:');
 const b = +prompt('Введите число b:');
 
 if (a >= 0 && b >= 0)
-    console.log(`разность ${a} и ${b} составит ${a - b}`)
+{
+    console.log(`разность ${a} и ${b} составит ${a - b}`);
+}
 else if (a < 0 && b < 0)
-    console.log(`произведение ${a} и ${b} составит ${a * b}`)
+{
+    console.log(`произведение ${a} и ${b} составит ${a * b}`);
+}
 else
-    console.log(`сумма ${a} и ${b} составит ${a + b}`)
+{
+    console.log(`сумма ${a} и ${b} составит ${a + b}`);
+}
