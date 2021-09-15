7a185a1879a1158f12f4239be3098d1ead99df3b
Дмитрий <semu-dima@yandex.ru>
2021-09-15 19:35:33 +0300

    added task :four:

---
+++ Scripts/ScriptTask_4.js
@@ -0,0 +1,43 @@
+'use strict'
+
+const products = [
+    {
+        id: 3,
+        price: 127,
+        photos: [
+            "1.jpg",
+            "2.jpg",
+        ]
+    },
+    {
+        id: 5,
+        price: 499,
+        photos: []
+    },
+    {
+        id: 10,
+        price: 26,
+        photos: [
+            "3.jpg"
+        ]
+    },
+    {
+        id: 8,
+        price: 78,
+    }
+];
+
+const filteredProducts = products.filter(p => p.photos?.length > 0)
+console.log(filteredProducts);
+
+products.sort(function(a, b) {
+    if(a.price > b.price) {
+        return 1;
+    }
+    if(a.price < b.price){
+        return -1;
+    }
+    return 0;    
+});
+
+console.log(products);
