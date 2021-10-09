205b907314faba3e0941c35d70ff741a063b45ed
Дмитрий <semu-dima@yandex.ru>
2021-10-09 18:33:08 +0300

    modified the basket

--- app.js
+++ app.js
@@ -32,14 +32,60 @@ filterSizeWrap.addEventListener('click', function() {
 const btns = document.querySelectorAll('.featuredItems button');
 const counterEl = document.querySelector('.cartIconWrap span');
 const cartIconWrapEl = document.querySelector('.cartIconWrap');
-const recyclebin = document.querySelector('.recyclebin');
+const addedProducts = document.querySelector('.addedProducts');
+const array = [];
 
 btns.forEach(btn => {
-    btn.addEventListener('click', () => {
+    btn.addEventListener('click', event => {
         counterEl.textContent = Number(counterEl.textContent) + 1;
+
+        const productsNames = document.querySelectorAll('.product .productName');
+        const productsVolume = document.querySelectorAll('.product .volume');
+        const pricePerPiece = document.querySelectorAll('.product .pricePerPiece');
+        const totalPrices = document.querySelectorAll('.product .totalPrice');
+        const sumEl = document.querySelector('.sum span');
+        
+        let productEl = event.target?.parentNode?.parentNode?.nextElementSibling;
+        let productNotInBasket = productEl.querySelector('.featuredName').textContent;
+
+        if(productsNames.length === 0) {
+            addedProducts?.insertAdjacentHTML('beforeend', changeHTML(productEl));
+            sumEl.textContent = productEl.querySelector('.featuredPrice').textContent;
+            array.push(Number(sumEl.textContent.replace('$', '')));
+        }
+    
+        for (let index = 0; index < productsNames.length; index++) {
+            if(productsNames[index].textContent === productNotInBasket) {
+                productsVolume[index].textContent = Number(productsVolume[index].textContent) + 1;
+                totalPrices[index].textContent = `$${Number(productsVolume[index].textContent) * Number(pricePerPiece[index].textContent.replace('$', ''))}.00`;
+                array[index] = Number(totalPrices[index].textContent.replace("$", ""));
+                sumEl.textContent = "$" + getSum(array) + ".00";
+                break;
+            }
+
+            if(index === productsNames.length - 1) {
+                addedProducts?.insertAdjacentHTML('beforeend', changeHTML(productEl));
+                array.push(Number(productEl.querySelector('.featuredPrice').textContent.replace('$', '')));
+                sumEl.textContent = "$" + getSum(array) + ".00";
+            }
+        }
     });
 });
 
 cartIconWrapEl?.addEventListener('click', () => {
-    recyclebin?.classList.toggle('hidden');
+    if(Number(counterEl.textContent) != 0) {
+        document.querySelector('.recyclebin')?.classList.toggle('hidden');
+    }
 });
+
+function changeHTML(productEl) {
+    return `<div class=product><div class=productName.block>${productEl.querySelector('.featuredName').textContent}</div><div class=volume block>${1}</div><div class=pricePerPiece block>${productEl.querySelector('.featuredPrice').textContent}</div><div class=totalPrice block>${productEl.querySelector('.featuredPrice').textContent}</div></div>`;
+}
+
+function getSum(array) {
+    let sum = 0;
+    array.forEach(number => {
+        sum += number;
+    });
+    return sum;
+}
